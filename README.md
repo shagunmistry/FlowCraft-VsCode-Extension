b79b63b8ac719007c9410f6a76329da678c813c4
Shagun Mistry <shagun.mistry@hotmail.com>
2024-04-26 20:42:08 -0400

    FlowCraft extension first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# FlowCraft
+
+FlowCraft empowers developers to effortlessly convert code into visual flow diagrams. Simplify your workflow and enhance code comprehension by generating clear and concise flowcharts directly within your development environment.
+
+## Features
+
+- **Effortless Code Conversion**: Select a code block or an entire file and instantly generate a corresponding flow diagram.
+- **Flowchart Focus**: Currently supports flow diagrams, with potential future expansion to other diagram types.
+- **Seamless Integration**: Works seamlessly within VS Code, allowing you to visualize code flow without context switching.
+- **Improved Code Understanding**: Flow diagrams provide a visual representation of code logic, making it easier to understand complex structures.
+- **Enhanced Communication**: Flowcharts facilitate clear communication of code flow between developers and stakeholders.
+
+## Release Notes
+
+### 1.0.0
+
+This is the first release of FlowCraft. As such, it is a basic implementation of the core functionality. Future releases will include additional features and improvements.
+
+We welcome feedback and suggestions for future enhancements!
+
+---
+
+## Following extension guidelines
+
+Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
+
+- [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
+
+## Working with Markdown
+
+You can author your README using Visual Studio Code. Here are some useful editor keyboard shortcuts:
+
+- Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux).
+- Toggle preview (`Shift+Cmd+V` on macOS or `Shift+Ctrl+V` on Windows and Linux).
+- Press `Ctrl+Space` (Windows, Linux, macOS) to see a list of Markdown snippets.
+
+## For more information
+
+- [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
+- [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
+
+**Enjoy!**
---
+++ src/extension.ts
@@ -0,0 +1,234 @@
+import * as vscode from "vscode";
+
+const FLOWCRAFT_API_URL = "https://flowcraft-api-cb66lpneaq-ue.a.run.app";
+
+const GENERATE_FLOW_DIAGRAM = "flowcraft.generateFlowDiagram";
+const GENERATE_SELECTION_FLOW_DIAGRAM =
+  "flowcraft.generateFlowDiagramFromSelection";
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "flowcraft" is now active!');
+
+  let generateFlowDiagramDisposable = vscode.commands.registerCommand(
+    GENERATE_FLOW_DIAGRAM,
+    async () => {
+      vscode.window.withProgress(
+        {
+          location: vscode.ProgressLocation.Notification,
+          title: "Generating Diagram from FlowCraft",
+          cancellable: false,
+        },
+        (progress, _token) => {
+          progress.report({ increment: 0 });
+          return new Promise<void>((resolve) => {
+            const activeEditor = vscode.window.activeTextEditor;
+            if (!activeEditor) {
+              vscode.window.showErrorMessage(
+                "No active editor found. Please open a file to generate a diagram."
+              );
+              return;
+            }
+            const fileName = activeEditor.document.fileName;
+            const fileExtension = fileName.split(".").pop();
+
+            progress.report({ increment: 20 });
+
+            if (!fileExtension) {
+              vscode.window.showErrorMessage(
+                "Please save the file with a valid extension."
+              );
+              return;
+            }
+
+            const fileContent = activeEditor.document.getText();
+            if (fileContent.length === 0 || fileContent.length > 10000) {
+              vscode.window.showErrorMessage(
+                "The file content is either empty or too large (max 10,000 characters). If you have a large file, please contact us at https://flowcraft.app/support."
+              );
+              return;
+            }
+
+            const flowCraftApiUrl =
+              process.env.FLOWCRAFT_API_URL || FLOWCRAFT_API_URL;
+
+            let title = fileName.split("/").pop();
+            title = title?.replace(/\s/g, "_");
+            const body = {
+              title,
+              description: fileContent,
+              type: "flowchart",
+            };
+
+            progress.report({ increment: 40 });
+
+            fetch(`${flowCraftApiUrl}/diagrams/generate`, {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(body),
+            })
+              .then((response) => response.json())
+              .then((data: any) => {
+                progress.report({ increment: 80 });
+                console.log("RESPONSE Data: ", data.response);
+
+                const _res = data.response;
+                let diagram = _res.mermaid_code;
+                console.log("Diagram: ", diagram);
+
+                const inserted_diagram = _res.inserted_diagram;
+
+                console.log("Inserted Diagram: ", inserted_diagram);
+                if (
+                  inserted_diagram &&
+                  inserted_diagram.data &&
+                  inserted_diagram.data.length > 0
+                ) {
+                  progress.report({ increment: 100 });
+                  const diagramUrl = `https://flowcraft.app/vscode/${inserted_diagram.data[0].id}`;
+
+                  vscode.env.openExternal(vscode.Uri.parse(diagramUrl));
+
+                  vscode.window
+                    .showInformationMessage(
+                      "The diagram has been successfully generated. If the diagram does not open automatically, please click on the link below.",
+                      "Open Diagram"
+                    )
+                    .then((selection) => {
+                      if (selection === "Open Diagram") {
+                        vscode.env.openExternal(vscode.Uri.parse(diagramUrl));
+                      }
+                    });
+                } else {
+                  vscode.window.showErrorMessage(
+                    "An error occurred while generating the diagram. Please try again later."
+                  );
+                }
+
+                resolve();
+              })
+              .catch((error) => {
+                vscode.window.showErrorMessage(
+                  "An error occurred while generating the diagram. Please try again later."
+                );
+
+                console.error("Error: ", error);
+                return;
+              });
+          });
+        }
+      );
+    }
+  );
+
+  let generateSelectionDiagramDisposable = vscode.commands.registerCommand(
+    GENERATE_SELECTION_FLOW_DIAGRAM,
+    async () => {
+      vscode.window.withProgress(
+        {
+          location: vscode.ProgressLocation.Notification,
+          title: "Generating Diagram from FlowCraft",
+          cancellable: false,
+        },
+        (progress, _token) => {
+          progress.report({ increment: 0 });
+          return new Promise<void>((resolve) => {
+            const activeEditor = vscode.window.activeTextEditor;
+            if (!activeEditor) {
+              vscode.window.showErrorMessage(
+                "No active editor found. Please open a file to generate a diagram."
+              );
+              return;
+            }
+
+            const selection = activeEditor.document.getText(
+              activeEditor.selection
+            );
+
+            if (selection.length === 0 || selection.length > 10000) {
+              vscode.window.showErrorMessage(
+                "The selection is either empty or too large (max 10,000 characters). If you have a large selection, please contact us at https://flowcraft.app/support."
+              );
+              return;
+            }
+
+            const flowCraftApiUrl =
+              process.env.FLOWCRAFT_API_URL || FLOWCRAFT_API_URL;
+
+            let title = "FlowCraft_Diagram";
+            title = title.replace(/\s/g, "_");
+            const body = {
+              title,
+              description: selection,
+              type: "flowchart",
+            };
+
+            progress.report({ increment: 40 });
+
+            fetch(`${flowCraftApiUrl}/diagrams/generate`, {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(body),
+            })
+              .then((response) => response.json())
+              .then((data: any) => {
+                progress.report({ increment: 80 });
+                console.log("RESPONSE Data: ", data.response);
+
+                const _res = data.response;
+                let diagram = _res.mermaid_code;
+                console.log("Diagram: ", diagram);
+
+                const inserted_diagram = _res.inserted_diagram;
+
+                console.log("Inserted Diagram: ", inserted_diagram);
+                if (
+                  inserted_diagram &&
+                  inserted_diagram.data &&
+                  inserted_diagram.data.length > 0
+                ) {
+                  progress.report({ increment: 100 });
+                  const diagramUrl = `https://flowcraft.app/vscode/${inserted_diagram.data[0].id}`;
+
+                  vscode.env.openExternal(vscode.Uri.parse(diagramUrl));
+                  vscode.window
+                    .showInformationMessage(
+                      "The diagram has been successfully generated. If the diagram does not open automatically, please click on the link below.",
+                      "Open Diagram"
+                    )
+                    .then((selection) => {
+                      if (selection === "Open Diagram") {
+                        vscode.env.openExternal(vscode.Uri.parse(diagramUrl));
+                      }
+                    });
+                }
+
+                resolve();
+              })
+              .catch((error) => {
+                vscode.window.showErrorMessage(
+                  "An error occurred while generating the diagram. Please try again later."
+                );
+
+                console.error("Error: ", error);
+                return;
+              });
+          });
+        }
+      );
+    }
+  );
+
+  context.subscriptions.push(generateFlowDiagramDisposable);
+  context.subscriptions.push(generateSelectionDiagramDisposable);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}

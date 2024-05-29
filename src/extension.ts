4920698e4a8fb7697fd313ebad6d9b7a30ec345b
Shagun Mistry <shagun.mistry@hotmail.com>
2024-05-29 11:48:47 -0400

    chore: Update package.json for FlowCraft extension version and commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,38 @@
 import * as vscode from "vscode";
+import * as path from "path";
+import * as fs from "fs";
 
 const FLOWCRAFT_API_URL = "https://flowcraft-api-cb66lpneaq-ue.a.run.app";
 
 const GENERATE_FLOW_DIAGRAM = "flowcraft.generateFlowDiagram";
 const GENERATE_SELECTION_FLOW_DIAGRAM =
   "flowcraft.generateFlowDiagramFromSelection";
+const GENERATE_CLASS_DIAGRAM = "flowcraft.generateClassDiagram";
+const GENERATE_SELECTION_CLASS_DIAGRAM =
+  "flowcraft.generateClassDiagramFromSelection";
+
+async function getSelectionText() {
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    return "";
+  }
+
+  const selection = editor.selection;
+  const text = editor.document.getText(selection);
+
+  return text;
+}
+
+async function getCurrentOpenFileText() {
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    return "";
+  }
+
+  const text = editor.document.getText();
+
+  return text;
+}
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -55,7 +83,7 @@ export function activate(context: vscode.ExtensionContext) {
             const flowCraftApiUrl =
               process.env.FLOWCRAFT_API_URL || FLOWCRAFT_API_URL;
 
-            let title = fileName.split("/").pop();
+            let title = fileName.split("\\").pop();
             title = title?.replace(/\s/g, "_");
             const body = {
               title,
@@ -160,8 +188,9 @@ export function activate(context: vscode.ExtensionContext) {
             const flowCraftApiUrl =
               process.env.FLOWCRAFT_API_URL || FLOWCRAFT_API_URL;
 
-            let title = "FlowCraft_Diagram";
-            title = title.replace(/\s/g, "_");
+            let title = activeEditor.document.fileName || "Untitled";
+            const fileNameOnly = title.split("\\").pop() || "Untitled";
+            title = fileNameOnly.replace(/\s/g, "_");
             const body = {
               title,
               description: selection,
@@ -226,8 +255,192 @@ export function activate(context: vscode.ExtensionContext) {
     }
   );
 
+  let generateClassDiagramDisposable = vscode.commands.registerCommand(
+    GENERATE_CLASS_DIAGRAM,
+    async () => {
+      vscode.window.withProgress(
+        {
+          location: vscode.ProgressLocation.Notification,
+          title: "Generating Diagram from FlowCraft",
+          cancellable: false,
+        },
+        (progress, _token) => {
+          progress.report({ increment: 0 });
+          return new Promise<void>(async (resolve) => {
+            const fileContext = await getCurrentOpenFileText();
+
+            if (fileContext.length === 0 || fileContext.length > 10000) {
+              vscode.window.showErrorMessage(
+                "The file content is either empty or too large (max 10,000 characters). If you have a large file, please contact us at https://flowcraft.app/support."
+              );
+              return;
+            }
+
+            const flowCraftApiUrl =
+              process.env.FLOWCRAFT_API_URL || FLOWCRAFT_API_URL;
+
+            let title = `Class Diagram - ${new Date().toISOString()}`;
+            title = title?.replace(/\s/g, "_");
+            const body = {
+              title,
+              description: fileContext,
+              type: "classDiagram",
+            };
+
+            progress.report({ increment: 40 });
+
+            fetch(`${flowCraftApiUrl}/diagrams/generate`, {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(body),
+            })
+              .then((response) => response.json())
+              .then((data: any) => {
+                progress.report({ increment: 80 });
+                console.log("RESPONSE Data: ", data.response);
+
+                const _res = data.response;
+                let diagram = _res.mermaid_code;
+                console.log("Diagram: ", diagram);
+
+                const inserted_diagram = _res.inserted_diagram;
+
+                console.log("Inserted Diagram: ", inserted_diagram);
+                if (
+                  inserted_diagram &&
+                  inserted_diagram.data &&
+                  inserted_diagram.data.length > 0
+                ) {
+                  progress.report({ increment: 100 });
+                  const diagramUrl = `https://flowcraft.app/vscode/${inserted_diagram.data[0].id}`;
+
+                  vscode.env.openExternal(vscode.Uri.parse(diagramUrl));
+
+                  vscode.window
+                    .showInformationMessage(
+                      "The diagram has been successfully generated. If the diagram does not open automatically, please click on the link below.",
+                      "Open Diagram"
+                    )
+                    .then((selection) => {
+                      if (selection === "Open Diagram") {
+                        vscode.env.openExternal(vscode.Uri.parse(diagramUrl));
+                      }
+                    });
+                }
+
+                resolve();
+              })
+              .catch((error) => {
+                vscode.window.showErrorMessage(
+                  "An error occurred while generating the diagram. Please try again later."
+                );
+
+                console.error("Error: ", error);
+                return;
+              });
+          });
+        }
+      );
+    }
+  );
+
+  let generateSelectionClassDiagramDisposable = vscode.commands.registerCommand(
+    GENERATE_SELECTION_CLASS_DIAGRAM,
+    async () => {
+      vscode.window.withProgress(
+        {
+          location: vscode.ProgressLocation.Notification,
+          title: "Generating Diagram from FlowCraft",
+          cancellable: false,
+        },
+        (progress, _token) => {
+          progress.report({ increment: 0 });
+          return new Promise<void>(async (resolve) => {
+            const selection = await getSelectionText();
+
+            if (selection.length === 0 || selection.length > 10000) {
+              vscode.window.showErrorMessage(
+                "The selection is either empty or too large (max 10,000 characters). If you have a large selection, please contact us at https://flowcraft.app/support."
+              );
+              return;
+            }
+
+            const flowCraftApiUrl =
+              process.env.FLOWCRAFT_API_URL || FLOWCRAFT_API_URL;
+
+            let title = `Class Diagram - ${new Date().toISOString()}`;
+            title = title?.replace(/\s/g, "_");
+            const body = {
+              title,
+              description: selection,
+              type: "classDiagram",
+            };
+
+            progress.report({ increment: 40 });
+
+            fetch(`${flowCraftApiUrl}/diagrams/generate`, {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(body),
+            })
+              .then((response) => response.json())
+              .then((data: any) => {
+                progress.report({ increment: 80 });
+                console.log("RESPONSE Data: ", data.response);
+
+                const _res = data.response;
+                let diagram = _res.mermaid_code;
+                console.log("Diagram: ", diagram);
+
+                const inserted_diagram = _res.inserted_diagram;
+
+                console.log("Inserted Diagram: ", inserted_diagram);
+                if (
+                  inserted_diagram &&
+                  inserted_diagram.data &&
+                  inserted_diagram.data.length > 0
+                ) {
+                  progress.report({ increment: 100 });
+                  const diagramUrl = `https://flowcraft.app/vscode/${inserted_diagram.data[0].id}`;
+
+                  vscode.env.openExternal(vscode.Uri.parse(diagramUrl));
+
+                  vscode.window
+                    .showInformationMessage(
+                      "The diagram has been successfully generated. If the diagram does not open automatically, please click on the link below.",
+                      "Open Diagram"
+                    )
+                    .then((selection) => {
+                      if (selection === "Open Diagram") {
+                        vscode.env.openExternal(vscode.Uri.parse(diagramUrl));
+                      }
+                    });
+                }
+
+                resolve();
+              })
+              .catch((error) => {
+                vscode.window.showErrorMessage(
+                  "An error occurred while generating the diagram from the selection. Please try again later."
+                );
+
+                console.error("Error: ", error);
+                return;
+              });
+          });
+        }
+      );
+    }
+  );
+
   context.subscriptions.push(generateFlowDiagramDisposable);
   context.subscriptions.push(generateSelectionDiagramDisposable);
+  context.subscriptions.push(generateClassDiagramDisposable);
+  context.subscriptions.push(generateSelectionClassDiagramDisposable);
 }
 
 // This method is called when your extension is deactivated
